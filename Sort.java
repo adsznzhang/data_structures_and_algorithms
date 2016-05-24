f8c5679e369037ea7b6849acb012f522865a3169
Yu <xyu974@hotmail.com>
2016-05-24 18:05:59 +0800

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -22,10 +22,19 @@ public class Sort {
 	 * Executes the insertion sort algorithm 
 	 * sorting the argument array. 
 	 * There is no return as the parameter is to be mutated. 
-	 * @param a - the array of long integers to be sorted
+	 * @param a the array of long integers to be sorted
 	 **/
 	public void insertionSort(long[] a) {
-		
+		for (int j = 1; j < a.length; j++){
+			long key = a[j];
+			int i = j - 1;
+			while(i >= 0 && a[i] > key){
+				a[i+1] = a[i];
+				i--;
+			}
+			a[i+1] = key;
+			count++;
+		}
 	}
 	
 	/**
