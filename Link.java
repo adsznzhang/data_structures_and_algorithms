166101ef9d2ce06efd33e82de476676af988a488
Yu <xyu974@hotmail.com>
2016-06-11 21:00:21 +0800

    Update Link.java

--- Link.java
+++ Link.java
@@ -1,9 +1,27 @@
+/**
+ * A link used to (recursively) construct a linked list.
+ * @author xyu974
+ * @version 1.0
+ */
 public class Link {
-	Object item;		//the item stored in this link
-	Link successor;		//the link stored in this link
 	
-	Link(Object c, Link s) {
-		item = c;
+	/**
+	 * Item stored in this link. 
+	 */
+	Object item;
+	
+	/**
+	 * Reference to the rest of the list. 
+	 */
+	Link successor;
+	
+	/**
+	 * Create a new link. 
+	 * @param c
+	 * @param s
+	 */
+	public Link(Object o, Link s){
+		item = o;
 		successor = s;
 	}
 }
